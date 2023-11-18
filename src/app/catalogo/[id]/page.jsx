a56c4f7db8ccc12c1698dddf9f4af250e7c27e1e
Catu Hardoy <catalinahardoy@gmail.com>
2023-11-18 16:52:49 -0300

    catalogo-id

--- src/app/catalogo/[id]/page.jsx
+++ src/app/catalogo/[id]/page.jsx
@@ -6,26 +6,33 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Divider } from '@mui/material';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useLayoutEffect } from 'react';
 import { useParams } from 'next/navigation';
 
 const CarDescription = () => {
   const useparams = useParams()
-  console.log(useparams)
+  // console.log(useparams)
   const carId = useparams.id; 
+  console.log(carId)
  
   const router = useRouter();
 
   const [car, setCar] = useState(null);
 
+const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  useEffect(() => {
+
+
+
+
+  useLayoutEffect(() => {
     const fetchCarData = async () => {
       try {
         if (carId) {
           const response = await fetch(`http://localhost:3000/api/cars/${carId}`);
           const data = await response.json();
-          setCar(data);
+          console.log(data)
+          setCar(data.car);
         }
       } catch (error) {
         console.error('Error al cargar los datos del auto', error);
@@ -41,13 +48,25 @@ const CarDescription = () => {
   
   return (
     <div className={styles.container}>
-       
-      
       <div className={styles.left}>
         {car ? (
-          <div className={styles.item} key={car._id}>
-             <img src='https://images.pexels.com/photos/707046/pexels-photo-707046.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt={car.name} className={styles.img} />
-          </div>
+      <div className={styles.item} key={car._id}>
+        <div className={styles.imgContainer}>
+        <img src={car.photoURLs[currentImageIndex].url} alt={car.name} className={styles.img} />
+        </div>
+        
+      <div className={styles.thumbnailContainer}>
+        {car.photoURLs.map((photo, index) => (
+          <img
+            key={index}
+            src={photo.url}
+            alt={`Thumbnail ${index + 1}`}
+            className={styles.thumbnail}
+            onClick={() => setCurrentImageIndex(index)}
+          />
+        ))}
+      </div>
+    </div>
         ) : (
           <div>no se encontraron datos de este auto</div>
         )}
@@ -56,21 +75,18 @@ const CarDescription = () => {
         {car ? (
           <div className={styles.content}>
             <Image src = '/LOGO_AUTOBAIRES_03.png' alt='autobaires' width={100} height={100} className={styles.logo}/>
-            <h1 className={styles.name}>{car.name}</h1>
-            <p className={styles.name}>Año: {car.year}</p>
-            <p className={styles.year}>Kilometros: {car.km}</p>
-
-            <p className={styles.desc}>Descripción: {car.desc}</p>
-            <br />
-            <Divider/>
-            <p className={styles.price}>Precio: {car.price}</p>
+          <h1 className={styles.name}><span className={styles.title}></span> {car.name}</h1>
+          <p className={styles.name}><span className={styles.title}>Año:</span> {car.year}</p>
+          <p className={styles.year}><span className={styles.title}>Kilometros:</span> {car.km}</p>
+          <p className={styles.desc}><span className={styles.title}>Descripción:</span> {car.description}</p>
+          <p className={styles.price}>${car.price}</p>
             
               <Link href='http://wa.me/qr/A2RTH4IJVTBQA1' target="_blank" rel="noopener noreferrer">
                 <button className={styles.button}>CONSULTAR</button>
               </Link>
               <br />
-              <br />
-              <br />
+              
+             
               <Link href='/catalogo'>
                 <button className={styles.buttonBack}>VOLVER</button>
               </Link>
