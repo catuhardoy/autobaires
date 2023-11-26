ad2f5b791247616133c0d5d5123d176891eb6162
Catu Hardoy <catalinahardoy@gmail.com>
2023-11-26 17:38:45 -0300

    fix bug video

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -4,7 +4,7 @@ const Video = () => {
   return (
     
     <div style={{ width: '100vw'  }}>
-        <video control width="100%" height={600} autoPlay muted loop controls>
+        <video width="100%" height={600} autoPlay muted loop controls>
           <source src="AutobairesVideo.mp4" type="video/mp4" />
         </video>
     </div>
